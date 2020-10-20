1c082b88003b0525b68828f8578cd59899f93542
siwon <swleeca@gmail.com>
2020-10-19 22:49:33 -0400

    '2020-10-19'

--- resources/views/layouts/modal_indi_incomplete.blade.php
+++ resources/views/layouts/modal_indi_incomplete.blade.php
@@ -1,5 +1,6 @@
 <?use Carbon\Carbon;?>
 
+
 <tr>
 	<td>{{$data1->upc}} </td>
 	<td>{{$data1->brand}}  </td>
@@ -137,213 +138,218 @@ break;
 
 <tr id="in{{$cnt}}" class="collapse">
 <td colspan=4>
+{{-- edit 내용 시작 --}}
+
+<div class="well well-lg">
+	<table class="table">
+		<tr>
+			<td width=200px>Upc</td>
+			<td @if($alt==11) class="has-error"@endif><input name="upc" class="form-control"	type=text value="{{$data1->upc}}"></input></td>
+			<td>8,12 or 13 digits only, The current length :<font color=red> {{strlen($data1->upc)}} </font> </td>
+		</tr>
+		<tr>
+			<td width=200px>Brand</td>
+			<td @if($alt==10) class="has-error"@endif><input name="brand" class="form-control" type=text value="{{$data1->brand}}"></input>
+			</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td width=200px>Description</td>
+			<td><input name="description" class="form-control" type=text value="{{$data1->description}}"></input></td>
+			<td></td>
+		</tr>
 
 
-	<div class="well well-lg">
-		<table class="table">
-			<tr>
-				<td width=200px>Upc</td>
-				<td <?if($alt==11){echo"class=has-error";}?>><input name="upc" class="form-control"
-						type=text value="{{$data1->upc}}"></input></td>
-				<td>8,12 or 13 digits only, The current length :
-					<?echo "<font color=red>".strlen($data1->upc)."</font>";?>
-				</td>
-			</tr>
-			<tr>
-				<td width=200px>Brand</td>
-				<td <?if($alt==10){echo"class=has-error";}?>><input name="brand" class="form-control"
-						type=text value="{{$data1->brand}}"></input>
-				</td>
-				<td></td>
-			</tr>
-			<tr>
-				<td width=200px>Description</td>
-				<td><input name="description" class="form-control" type=text
-						value="{{$data1->description}}"></input></td>
-				<td></td>
-			</tr>
+		<tr>
+			<td width=200px>Short_desc</td>
+		<td @if($alt==8) class="has-error"@endif><input id="{{$cnt}}desc" name="short_desc1"
+					maxlength=24 class="form-control" type=text value="{{$data1->short_desc}}"
+		onkeyup=display_strlen{{$cnt}}()></input></td>
+			<td>limits up to 24 characters <font color=red><span id="{{$cnt}}spit"></span> </font>
+			</td>
 
 
-			<tr>
-				<td width=200px>Short_desc</td>
-				<td <?if($alt==8){echo"class=has-error";}?>><input id="<?=$cnt?>desc" name="short_desc1"
-						maxlength=24 class="form-control" type=text value="{{$data1->short_desc}}"
-						onkeyup=display_strlen<?=$cnt?>()></input></td>
-				<td>limits up to 24 characters <font color=red><span id="<?=$cnt?>spit"></span> </font>
-				</td>
+		</tr>
+		<script>
+		function display_strlen{{$cnt}}() {
+			var x = document.getElementById("{{$cnt}}desc").value.length;
+			document.getElementById("{{$cnt}}spit").innerHTML = x;
+		}
+		</script>
 
 
-			</tr>
-			<script>
-			function display_strlen<?=$cnt?>() {
-				var x = document.getElementById("<?=$cnt?>desc").value.length;
-				document.getElementById("<?=$cnt?>spit").innerHTML = x;
-			}
-			</script>
+		<tr>
+			<td width=200px>Category</td>
+			<td @if($alt==2 or $alt==16 or $alt==17 or $alt==18) class="has-error"@endif>
+				<input name="category" maxlength=2 class="form-control" type=text value="@if($data1->category_full){{$data1->category_full}}@elseif(strlen($data1->category)==1){{'0'.$data1->category}}
+				@elseif(strlen($data1->category)==2){{$data1->category}}@endif"></input>
+			</td>
+			<td>2 characters, example : "02", verified : <font color=red> {{$data1->category}}</font>
+			</td>
+		</tr>
+
+		<tr>
+			<td width=200px>Sub_category</td>
+			<td @if($alt==1 or $alt==12 or $alt==13 or $alt==14 or $alt==15 or $alt==23 or
+				$alt==26) class="has-error"@endif >
+				<input name="subcate" maxlength=3 class="form-control form-control-danger" type=text
+					value="@if($data1->subcat_full){{$data1->subcat_full}}
+					@elseif(strlen($data1->subcategory)==1)
+					{{'00'.$data1->subcategory }}
+					@elseif(strlen($data1->subcategory)==2)
+					{{'0'.$data1->subcategory}}
+					@endif"></input>
+			</td>
+			<td>3 characters, example : "002" , verified : <font color=red> {{$data1->subcategory}}	</font>
+			</td>
+		</tr>
 
 
-			<tr>
-				<td width=200px>Category</td>
-				<td <?if($alt==2 or $alt==16 or $alt==17 or $alt==18){echo"class=has-error";}?>>
-					<input name="category" maxlength=2 class="form-control" type=text value="<?
-						if($data1->category_full){
+		<tr>
+			<td width=200px>Benefit UOM</td>
+			<td @if($alt==7 or $alt==19) class="has-error"@endif >
+				<select name="benefit_uom_wow" maxlength=3 class="form-control">
+					<option @if ($data1->benefit_uom_wow=='' ) "selected" @endif value="">Select One
+					</option>
+					<option @if ($data1->benefit_uom_wow=='$$$' ) "selected" @endif	value="$$$">$$$
+					</option>
+					<option @if ($data1->benefit_uom_wow=='BAG') "selected" @endif value="BAG">BAG
+					</option>
+					<option @if ($data1->benefit_uom_wow=='CAN') "selected" @endif value="CAN">CAN
+					</option>
 
-						echo  $data1->category_full;
+					<option @if ($data1->benefit_uom_wow=='CBL') "selected" @endif value="CBL">CBL
+					</option>
+					<option @if ($data1->benefit_uom_wow=='CTR') "selected" @endif value="CTR">CTR
+					</option>
 
-						}else if(strlen($data1->category)==1){
+					<option @if ($data1->benefit_uom_wow=='DOZ') "selected" @endif value="DOZ">DOZ
+					</option>
+					<option @if ($data1->benefit_uom_wow=='GAL') "selected" @endif value="GAL">GAL
+					</option>
 
-						echo '0'.$data1->category; }else if(strlen($data1->category)==2){ echo $data1->category; } ?>"></input>
-				</td>
-				<td>2 characters, example : "02", verified : <font color=red> {{$data1->category}}</font>
-				</td>
-			</tr>
-
-			<tr>
-				<td width=200px>Sub_category</td>
-				<td <?if($alt==1 or $alt==12 or $alt==13 or $alt==14 or $alt==15 or $alt==23 or
-					$alt==26){echo"class=has-error";}?> >
-					<input name="subcate" maxlength=3 class="form-control form-control-danger" type=text
-						value="<?
-							if($data1->subcat_full){
-
-							echo  $data1->subcat_full;
-
-							}else if(strlen($data1->subcategory)==1){
-
-							echo '00'.$data1->subcategory; }else if(strlen($data1->subcategory)==2){ echo '0'.$data1->subcategory; } ?>"></input>
-				</td>
-				<td>3 characters, example : "002" , verified : <font color=red> {{$data1->subcategory}}
-					</font>
-				</td>
-			</tr>
- 
-
-			<tr>
-				<td width=200px>Benefit UOM</td>
-				<td <?if($alt==7 or $alt==19){echo"class=has-error";}?> >
-					<select name="benefit_uom_wow" maxlength=3 class="form-control">
-						<option <?if ($data1->benefit_uom_wow=='' ){echo "selected" ;}?> value="">Select
-							One
-						</option>
-						<option <?if ($data1->benefit_uom_wow=='$$$' ){echo "selected" ;}?>
-							value="$$$">$$$
-						</option>
-						<option <?if ($data1->benefit_uom_wow=='BAG'){echo "selected" ;}?> value="BAG">BAG
-						</option>
-						<option <?if ($data1->benefit_uom_wow=='CAN'){echo "selected" ;}?> value="CAN">CAN
-						</option>
-
-						<option <?if ($data1->benefit_uom_wow=='CBL'){echo "selected" ;}?> value="CBL">CBL
-						</option>
-						<option <?if ($data1->benefit_uom_wow=='CTR'){echo "selected" ;}?> value="CTR">CTR
-						</option>
-
-						<option <?if ($data1->benefit_uom_wow=='DOZ'){echo "selected" ;}?> value="DOZ">DOZ
-						</option>
-						<option <?if ($data1->benefit_uom_wow=='GAL'){echo "selected" ;}?> value="GAL">GAL
-						</option>
-
-						<option <?if ($data1->benefit_uom_wow=='HGL'){echo "selected" ;}?> value="HGL">HGL
-						</option>
+					<option @if ($data1->benefit_uom_wow=='HGL') "selected" @endif value="HGL">HGL
+					</option>
 
 
-						<option <?if ($data1->benefit_uom_wow=='JBG'){echo "selected" ;}?> value="JBG">JBG
-						</option>
-						<option <?if ($data1->benefit_uom_wow=='LB'){echo "selected" ;}?> value="LB">LB
-						</option>
+					<option @if ($data1->benefit_uom_wow=='JBG') "selected" @endif value="JBG">JBG
+					</option>
+					<option @if ($data1->benefit_uom_wow=='LB') "selected" @endif value="LB">LB
+					</option>
 
-						<option <?if ($data1->benefit_uom_wow=='OZ'){echo "selected" ;}?> value="OZ">OZ
-						</option>
-						<option <?if ($data1->benefit_uom_wow=='PKG'){echo "selected" ;}?> value="PKG">PKG
-						</option>
-						<option <?if ($data1->benefit_uom_wow=='QT'){echo "selected" ;}?> value="QT">QT
-						</option>
+					<option @if ($data1->benefit_uom_wow=='OZ') "selected" @endif value="OZ">OZ
+					</option>
+					<option @if ($data1->benefit_uom_wow=='PKG') "selected" @endif value="PKG">PKG
+					</option>
+					<option @if ($data1->benefit_uom_wow=='QT') "selected" @endif value="QT">QT
+					</option>
 
 
 
-					</select>
-				</td>
-				<td>$$$,BAG,CAN,CBL,CTR,DOZ,GAL,HGL,JBG,LB,OZ,PKG,QT only </td>
-			</tr>
-			<tr>
-				<td width=200px>Effective Date</td>
-				<td <?if($alt==3){echo"class=has-error";}?>><input name="verify_date" class="form-control"
-						type=text value="{{$data1->verify_date}}"></input></td>
-				<td>YYYY-MM-DD, example : "2017-01-31"</td>
-			</tr>
-			<tr>
-				<td width=200px>End Date</td>
-				<td <?if($alt==4){echo"class=has-error";}?>><input name="end_date" class="form-control"
-						type=text value="<?if($data1->end_date){echo $data1->end_date;}else{echo "
-						2024-12-31";} ?>"></input></td>
-				<td>"2024-12-31" if it is an approved item.</td>
-			</tr>
-			<tr>
-				<td width=200px>Benefit QT</td>
-				<td <?if($alt==5){echo"class=has-error";}?>><input name="benefit_qt" class="form-control"
-						type=text value="{{$data1->benefit_qt}}"></input></td>
-				<td> The issued QTY deducting unit</td>
-			</tr>
-			<tr>
-				<td width=200px>PLU Indicator</td>
-				<td <?if($alt==6){echo"class=has-error";}?>>
-					<label class="radio-inline"><input type="radio" name="plu_indicator" value="P" <?if
-							($data1->plu_indicator=='P' or strlen($data1->upc)==4 or
-							strlen($data1->upc)==5){echo "checked" ;}?>>P : PLU</label>
-					<label class="radio-inline"><input type="radio" name="plu_indicator" value="U" <?if
-							($data1->plu_indicator=='U' or strlen($data1->upc)==12 or
-							strlen($data1->upc)==13){echo "checked" ;}?>>U : UPC</label>
+				</select>
+			</td>
+			<td>$$$,BAG,CAN,CBL,CTR,DOZ,GAL,HGL,JBG,LB,OZ,PKG,QT only </td>
+		</tr>
+		<tr>
+			<td width=200px>Effective Date</td>
+			<td @if($alt==3) class="has-error"@endif><input name="verify_date" class="form-control"
+					type=text value="{{$data1->verify_date}}"></input></td>
+			<td>YYYY-MM-DD, example : "2017-01-31"</td>
+		</tr>
+		<tr>
+			<td width=200px>End Date</td>
+			<td @if($alt==4) class="has-error"@endif><input name="end_date" class="form-control"
+					type=text value="@if($data1->end_date) {{$data1->end_date}}   @else 2024-12-31 @endif "></input></td>
+			<td>"2024-12-31" if it is an approved item.</td>
+		</tr>
+		<tr>
+			<td width=200px>Benefit QT</td>
+			<td @if($alt==5) class="has-error"@endif><input name="benefit_qt" class="form-control"
+					type=text value="{{$data1->benefit_qt}}"></input></td>
+			<td> The issued QTY deducting unit</td>
+		</tr>
+		<tr>
+			<td width=200px>PLU Indicator</td>
+			<td @if($alt==6) class="has-error"@endif>
+				<label class="radio-inline">
+					<input type="radio" name="plu_indicator" value="P" 
+					@if($data1->plu_indicator == 'P' or strlen($data1->upc) == 4 or
+						strlen($data1->upc)==5)
+						 checked
+						@endif/>P : PLU</label>
+				<label class="radio-inline"><input type="radio" name="plu_indicator" value="U" 
+					@if($data1->plu_indicator=='U' or strlen($data1->upc)==12 or
+						strlen($data1->upc)==13) 
+						checked 
+						@endif />U : UPC</label>
 
 
 
 
-				</td>
+			</td>
 
-				<td>P : PLU, U : UPC </td>
-			</tr>
-			<tr>
-				<td width=200px>Broad Band</td>
-				<td <?if($alt==9 or $alt==24 or $alt==25){echo"class=has-error";}?>>
+			<td>P : PLU, U : UPC </td>
+		</tr>
+		<tr>
+			<td width=200px>Broad Band</td>
+			<td @if($alt==9 or $alt==24 or $alt==25) class="has-error"@endif>
 
-					<label class="radio-inline"><input type="radio" name="broadband_flag" value="0" <?if
-							($data1->broadband_flag==0){echo "checked" ;}?>>No : 0</label><label
-						class="radio-inline"><input type="radio" name="broadband_flag" value="1" <?if
-							($data1->broadband_flag==1){echo "checked" ;}?>>Yes : 1</label>
-
-
-
-
-				</td>
-
-				<td>1: 2-0,1,2,3 | 3-0,1 | 5-0,1,2 | 8-0,1,2,3 | 9-0,1 | 12-0,1,2,3 |13-0,1 |
-					16-0,1,2,3,7,8
-					| | 19-0,1,2,3 | 51-0,1 | 52-0,1 | 53-0,1 | 54-0,2<br>
-					0: 2-4 | 6-1,2,3 | 51-3,6,9,11,201,202 |
-					52-4,6,9,11,12,14,19,22,25,27,201,202,203,204,205 | 53-5</td>
-			</tr>
-			<tr>
-				<td width=200px>High Cost</td>
-				<td>
+				<label class="radio-inline"><input type="radio" name="broadband_flag" value="0" 
+					@if($data1->broadband_flag==0)
+					checked
+					 @endif />No : 0</label><label
+					class="radio-inline"><input type="radio" name="broadband_flag" value="1" 
+					@if($data1->broadband_flag==1)
+					 checked 
+					 @endif />Yes : 1</label>
 
 
-					<label class="radio-inline"><input type="radio" name="high_cost" value="0" <?if
-							($data1->high_cost==0 or $data1->high_cost=='' ){echo "checked"
-							;}?>>0:Non-High
-						Cost</label><label class="radio-inline"><input type="radio" name="high_cost"
-							value="1" <?if ($data1->high_cost==1){echo "checked" ;}?>>1:High Cost</label>
 
-				</td>
 
-				<td>0:Non-High Cost, 1:High Cost</td>
-			</tr>
-			<tr>
-				<td colspan=3 align=center>
-					<button class="btn btn-success btn-sm" type=submit>Submit</button>
-				</td>
-			</tr>
-		</table>
+			</td>
 
-	</div>
+			<td>1: 2-0,1,2,3 | 3-0,1 | 5-0,1,2 | 8-0,1,2,3 | 9-0,1 | 12-0,1,2,3 |13-0,1 |
+				16-0,1,2,3,7,8
+				| | 19-0,1,2,3 | 51-0,1 | 52-0,1 | 53-0,1 | 54-0,2<br>
+				0: 2-4 | 6-1,2,3 | 51-3,6,9,11,201,202 |
+				52-4,6,9,11,12,14,19,22,25,27,201,202,203,204,205 | 53-5</td>
+		</tr>
+		<tr>
+			<td width=200px>High Cost</td>
+			<td>
+
+
+				<label class="radio-inline"><input type="radio" name="high_cost" value="0" 
+					@if($data1->high_cost==0 or $data1->high_cost=='' )
+						 checked
+						@endif />0:Non-High	Cost</label>
+						<label class="radio-inline">
+						<input type="radio" name="high_cost" value="1" 
+						@if($data1->high_cost==1)
+						 checked 
+						@endif />1:High Cost</label>
+
+			</td>
+
+			<td>0:Non-High Cost, 1:High Cost</td>
+		</tr>
+		<tr>
+			<td colspan=3 align=center>
+				<button class="btn btn-success btn-sm" type=submit>Submit</button>
+			</td>
+		</tr>
+	</table>
+
+</div>
+
+
+
+
+{{-- edit 내용 끝 --}}
+
+
+
+
 </td>
 </tr>
 
--- resources/views/processor.blade.php
+++ resources/views/processor.blade.php
@@ -6,7 +6,6 @@
 @section('content')
    
  
- 
 
 <div class="px-3  py-2">
     <div class="px-10 py-4  text-3xl inline"  > <i class="nav-icon fas fa-sync-alt  text-3xl" ></i> DATA for WOW/SOAR
@@ -36,33 +35,142 @@
 //dd($errors['error4']);
 
 ?>
-
- 
- 
+<tbody>
 
 
- <?
-$cnt=100 ;
-$i=0;
+
+ <? $cnt=100 ; $i=0;
 ?> 
-@foreach($errs['error4'] as $data1 )
-
+@foreach($errs['error1'] as $data1 )
 <?
-
- 
-$cnt=$cnt+1;
-$i=$i+1;
+$cnt=$cnt+1; $i=$i+1;
 ?>
- 
-   @include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
-
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
 @endforeach
 
+<? $cnt=200 ; $i=0;
+?> 
+@foreach($errs['error2'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
+
+<? $cnt=300 ; $i=0;
+?> 
+@foreach($errs['error3'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
+
+<? $cnt=400 ; $i=0;
+?> 
+@foreach($errs['error4'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
+
+<? $cnt=500 ; $i=0;
+?> 
+@foreach($errs['error5'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
+
+<? $cnt=600 ; $i=0;
+?> 
+@foreach($errs['error6'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
 
  
- 
- 
+<? $cnt=700 ; $i=0;
+?> 
+@foreach($errs['error7'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
 
+<? $cnt=800 ; $i=0;
+?> 
+@foreach($errs['error8'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
+
+<? $cnt=900 ; $i=0;
+?> 
+@foreach($errs['error9'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
+
+<? $cnt=1000 ; $i=0;
+?> 
+@foreach($errs['error10'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
+
+<? $cnt=1100 ; $i=0;
+?> 
+@foreach($errs['error11'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
+
+<? $cnt=1200 ; $i=0;
+?> 
+@foreach($errs['error12'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
+
+<? $cnt=1300 ; $i=0;
+?> 
+@foreach($errs['error13'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
+
+<? $cnt=1400 ; $i=0;
+?> 
+@foreach($errs['error14'] as $data1 )
+<?
+$cnt=$cnt+1; $i=$i+1;
+?>
+@include('layouts.modal_indi_incomplete', compact('data1','cnt','i'))
+@endforeach
+
+  
+ 
+ 
+ 
+</tbody>
  </table>
 
  
